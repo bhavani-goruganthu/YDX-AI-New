db745867b1b12f72459226148e02ca63b866e1ac
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-02-13 21:21:28 -0800

    notes, video resized

--- src/pages/YDXHome.js
+++ src/pages/YDXHome.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import YouTube from 'react-youtube';
+import Draggable from 'react-draggable';
 import '../assets/css/home.css';
 
 const YDXHome = () => {
   const opts = {
-    height: '350',
-    width: '560',
+    height: '260',
+    width: '450',
     playerVars: {
       autoplay: 0,
       enablejsapi: 1,
@@ -29,20 +30,23 @@ const YDXHome = () => {
         </div>
         <div className="col">
           <div className="notes-bg">
-            <div className="my-auto align-items-center pt-2 px-3">
-              <h5 className="text-white">Notes:</h5>
+            <div className="my-auto align-items-center pt-1 px-3">
+              <h6 className="text-white">Notes:</h6>
             </div>
             <div className="mx-auto my-auto notes-textarea align-items-center border rounded">
               <textarea
                 className="form-control border rounded"
                 style={{ resize: 'none' }}
-                rows="9"
+                rows="7"
                 id="notes"
                 name="notes"
               ></textarea>
             </div>
-            <div className="mx-auto text-center mt-3">
-              <button type="button" className="btn rounded bg-warning">
+            <div className="mx-auto text-center mt-2">
+              <button
+                type="button"
+                className="btn rounded btn-sm text-white primary-btn-color"
+              >
                 <i className="fa fa-save" />
                 <b> Save</b>
               </button>
@@ -50,17 +54,46 @@ const YDXHome = () => {
           </div>
         </div>
       </div>
-      <div className="d-flex justify-content-around">
-        <button type="button" className="btn rounded bg-warning">
-          <i className="fa fa-play" /> <i className="fa fa-pause" />
-          <b> Play/Pause</b>
-        </button>
-        <button type="button" className="btn rounded bg-warning">
-          <i className="fa fa-plus"></i>
-          <b> Insert New AD</b>
-        </button>
-      </div>
+
       <hr />
+      <div
+        className="row ml-5 mb-4 mt-5 p-2"
+        style={{
+          height: 25,
+          width: 1000,
+          backgroundColor: '#fff4de',
+          border: '1.8px #bd805e solid',
+          borderRadius: '10px',
+          position: 'relative',
+        }}
+      >
+        <div style={{ width: 1000, position: 'absolute' }}>
+          {/* ProgressBar */}
+          <Draggable
+            axis="x"
+            bounds={{ left: 10, right: 950 }}
+            // defaultPosition={{ x: -3, y: 0 }}
+            // position={draggableTime}
+            // onDrag={dragProgressBar}
+            // onStop={stopProgressBar}
+          >
+            <div
+              id="progress_bar_timeline"
+              tabIndex={0}
+              style={{
+                float: 'left',
+                cursor: 'ew-resize',
+                width: '3px',
+                height: '85px',
+                backgroundColor: '#7b2b2b',
+                position: 'absolute',
+                zIndex: 1,
+                marginTop: '-37px',
+              }}
+            ></div>
+          </Draggable>
+        </div>
+      </div>
     </div>
   );
 };
