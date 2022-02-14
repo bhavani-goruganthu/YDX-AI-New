50f2a2b97207d0f958f750cd1c34d9d9df1ba302
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-02-13 21:21:06 -0800

    dark theme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import YDXHome from './pages/YDXHome';
 const App = () => {
   return (
     <div>
-      <h2 className="mx-auto text-center mt-1">
+      <h2 className="mx-auto text-center mt-1 text-white">
         <u>Welcome to YouDescribeX</u>
       </h2>
       <YDXHome />
