17259eeab6a254d0b63c8a910028126d8a65b8b7
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-02-20 17:58:41 -0800

    edit component

---
+++ src/components/EditDescriptionComponent.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import '../assets/css/editAudioDesc.css';
+
+const EditDescriptionComponent = () => {
+  return (
+    <div className="edit-component text-white">
+      <p>dom</p>
+    </div>
+  );
+};
+
+export default EditDescriptionComponent;
