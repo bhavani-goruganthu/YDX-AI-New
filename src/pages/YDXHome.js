2181ce59c052bf78a8f894f0a55f3beb56e4de29
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-03-28 14:29:06 -0700

    added a modal for publish

--- src/pages/YDXHome.js
+++ src/pages/YDXHome.js
@@ -238,10 +238,50 @@ const YDXHome = () => {
           </button>
         </div>
         <div className="publish-buttons mx-4">
-          <button type="button" className="btn publish-bg text-white">
+          <button
+            type="button"
+            className="btn publish-bg text-white"
+            data-bs-toggle="modal"
+            data-bs-target="#publishModal"
+          >
             <i className="fa fa-upload" /> {'   '}
             Publish
           </button>
+        </div>
+      </div>
+      {/* <!-- Publish Modal --> Confirmation Modal - opens when user hits Publish button and asks for a confirmation*/}
+      <div className="modal fade text-dark" id="publishModal">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content mx-auto w-75">
+            {/* <!-- Modal Header --> */}
+            <div className="modal-header">
+              <h4 className="modal-title">Publish</h4>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+              ></button>
+            </div>
+            {/* <!-- Modal body --> */}
+            <div className="modal-body text-center">Are you sure?</div>
+            {/* <!-- Modal footer --> */}
+            <div className="modal-footer d-flex justify-content-center align-items-center">
+              <button
+                type="button"
+                className="btn primary-btn-color text-center m-1 text-white"
+                data-dismiss="modal"
+              >
+                Yes
+              </button>
+              <button
+                type="button"
+                className="btn primary-btn-color text-white"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
