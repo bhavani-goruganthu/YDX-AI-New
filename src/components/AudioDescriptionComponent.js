8191c06e386f611479a33688128c27c166d4d997
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-04-10 13:14:41 -0700

    destructured props & added draggable div

--- src/components/AudioDescriptionComponent.js
+++ src/components/AudioDescriptionComponent.js
@@ -1,14 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/css/audioDesc.css';
+import Draggable from 'react-draggable';
 import EditDescriptionComponent from './EditDescriptionComponent';
 
 const AudioDescriptionComponent = (props) => {
+  // destructuring props
+  const unitLength = props.unitLength;
+  // const clip_id = props.clip.clip_id;
+  const clip_sequence_num = props.clip.clip_sequence_num;
+  const clip_title = props.clip.clip_title;
+  const clip_description_type = props.clip.description_type;
+  const clip_description_text = props.clip.description_text;
+  const clip_playback_type = props.clip.playback_type;
+  const clip_start_time = props.clip.clip_start_time;
+  const clip_end_time = props.clip.clip_end_time;
+  const clip_duration = props.clip.clip_duration;
+  const clip_audio_path = props.clip.clip_audio_path;
+  const is_recorded = props.clip.is_recorded;
+  const recorded_audio_path = props.clip.recorded_audio_path;
+
   // toggle variable to show or hide the edit component.
   const [showEditComponent, setShowEditComponent] = useState(false);
-  const [clipPlaybackType, setClipPlayBackType] = useState(
-    props.clip_playback_type
-  );
-  const [clipTitle, setClipTitle] = useState(props.clip_title);
+  const [clipPlaybackType, setClipPlayBackType] = useState(clip_playback_type);
+  const [clipTitle, setClipTitle] = useState(clip_title);
+  const [adDraggableWidth, setAdDraggableWidth] = useState(0.0);
+  const [adDraggablePosition, setAdDraggablePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [adBgColor, setAdBgColor] = useState('');
+
+  useEffect(() => {
+    // set c
+    setAdDraggablePosition({ x: clip_start_time * unitLength, y: 0 });
+    setAdDraggableWidth(clip_duration * unitLength);
+    clipPlaybackType === 'inline'
+      ? setAdBgColor('var(--inline-color')
+      : setAdBgColor('var(--extended-color');
+  }, [unitLength, clip_start_time, clip_duration, clip_end_time]);
 
   return (
     <React.Fragment>
@@ -17,7 +46,7 @@ const AudioDescriptionComponent = (props) => {
         <div className="row align-items-center">
           <div className="col-2 component-column-width-1">
             <div className="mx-1 text-center">
-              <p>Audio Desc {props.clip_sequence_num}</p>
+              <p>Audio Desc {clip_sequence_num}</p>
               <input
                 type="text"
                 className="form-control form-control-sm ad-title-input text-center"
@@ -27,8 +56,8 @@ const AudioDescriptionComponent = (props) => {
               />
               <h6 className="mt-1 text-white">
                 Type:{' '}
-                {props.clip_description_type.charAt(0).toUpperCase() +
-                  props.clip_description_type.slice(1)}
+                {clip_description_type.charAt(0).toUpperCase() +
+                  clip_description_type.slice(1)}
               </h6>
             </div>
           </div>
@@ -39,13 +68,30 @@ const AudioDescriptionComponent = (props) => {
             </div>
           </div>
           <div className="col-8 component-column-width-3">
-            <div className="row component-timeline-div" />
+            <div className="row component-timeline-div">
+              <div id="ad-draggable-div" className="ad-draggable-div">
+                <Draggable
+                  axis="x"
+                  position={adDraggablePosition}
+                  bounds="parent"
+                >
+                  <div
+                    className="ad-timestamp-div"
+                    style={{
+                      width: adDraggableWidth,
+                      height: '18px',
+                      backgroundColor: adBgColor,
+                    }}
+                  ></div>
+                </Draggable>
+              </div>
+            </div>
             <div className="mx-5 mt-2">
               <div className="form-check form-check-inline">
                 <input
                   className="form-check-input"
                   type="radio"
-                  name={props.clip_sequence_num}
+                  name={clip_sequence_num}
                   id="radio1"
                   value="inline"
                   checked={clipPlaybackType === 'inline' ? true : false}
@@ -59,7 +105,7 @@ const AudioDescriptionComponent = (props) => {
                 <input
                   className="form-check-input"
                   type="radio"
-                  name={props.clip_sequence_num}
+                  name={clip_sequence_num}
                   id="radio2"
                   value="extended"
                   checked={clipPlaybackType === 'extended' ? true : false}
@@ -90,8 +136,11 @@ const AudioDescriptionComponent = (props) => {
       {/* Based on the state of the showEditComponent variable, the edit component will be displayed*/}
       {showEditComponent ? (
         <EditDescriptionComponent
-          clip_description_text={props.clip_description_text}
-          clip_start_time={props.clip_start_time}
+          clip_description_text={clip_description_text}
+          clip_start_time={clip_start_time}
+          is_recorded={is_recorded}
+          recorded_audio_path={recorded_audio_path}
+          clip_audio_path={clip_audio_path}
         />
       ) : (
         <> </>
